3fe318f2d6dd3d8fa9fe68049c77b7db0a4302b3
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-13 09:43:24 -0600

    add iteration 3

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,29 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  attr_reader :reunion, :fishing
+
+  def setup
+    @reunion = Reunion.new('denver')
+    @fishing = Activity.new('fishing', 30)
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_reunion_has_location
+    assert_equal 'denver', reunion.location
+  end
+
+  def test_it_has_array_of_activites
+    assert_instance_of Array, reunion.activities
+  end
+
+  def test_it_can_add_activites
+    reunion.add_activity(fishing)
+    assert_instance_of Array, reunion.activities
+    assert_instance_of Activity, reunion.activities[0]
+  end
 end
