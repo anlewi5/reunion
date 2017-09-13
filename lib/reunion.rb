0b00421d7d3210fc83d2b06bbc36ed31c08074a6
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-13 10:01:54 -0600

    start on iteration 4: make it find total cost and start on creating breakdown

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,4 +11,31 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    total_cost = 0
+    activities.each do |activity|
+      total_cost += activity.total_cost
+    end
+    total_cost
+  end
+
+  #not tested below
+  #goal: make hash of hashes
+  #<activity => <participant => owes, other_participant => owes>>
+
+  def debt
+    activity_hash = {}
+    activities.each do |activity|
+      owed_hash = make_owed_hash
+      activity_hash[activity] = owed_hash
+    end
+    activity_hash
+  end
+
+  def make_owed_hash(activity)
+    activity.participants.map do |participant|
+      {participant[:name] => participant[:amount]}
+    end
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -5,11 +5,12 @@ require './lib/reunion'
 
 class ReunionTest < Minitest::Test
 
-  attr_reader :reunion, :fishing
+  attr_reader :reunion, :fishing, :talking
 
   def setup
     @reunion = Reunion.new('denver')
     @fishing = Activity.new('fishing', 30)
+    @talking = Activity.new('talking', 70)
   end
 
   def test_it_exists
@@ -28,5 +29,25 @@ class ReunionTest < Minitest::Test
     reunion.add_activity(fishing)
     assert_instance_of Array, reunion.activities
     assert_instance_of Activity, reunion.activities[0]
+  end
+
+  def test_reunion_has_total_cost
+    reunion.add_activity(fishing)
+    reunion.add_activity(talking)
+    assert_equal 100, reunion.total_cost
+  end
+
+  def test_it_knows_how_much_all_participants_are_owed
+    # fishing = reunion.add_activity(fishing)
+    # talking = reunion.add_activity(talking)
+    #
+    # participant1_hash = {name: 'beth', amount: 5}
+    # participant2_hash = {name: 'aaron', amount: 10}
+    #
+    # fishing.add_participant(participant1_hash)
+    # fishing.add_participant(participant2_hash)
+    # talking.add_participant(participant1_hash)
+    # talking.add_participant(participant2_hash)
+
   end
 end
