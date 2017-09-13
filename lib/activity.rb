707d2d803b5607fc649b6938d7561aa048513e01
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-13 09:17:10 -0600

    add iteration 0

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,12 @@
 class Activity
+
+  attr_reader :name, :participant_hash, :participant, :amount_paid
+
+  def initialize(name, participant_hash)
+    @name = name
+    @participant_hash = participant_hash
+    @participant = participant_hash[:name]
+    @amount_paid = participant_hash[:amount]
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,31 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  attr_reader :activity
+
+  def setup
+    participant_hash = {name: 'beth', amount: 5}
+    @activity = Activity.new('fun times', participant_hash)
+  end
+
+  def test_it_exists
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_name
+    assert_equal 'fun times', activity.name
+  end
+
+  def test_it_stores_participants_as_hash
+    assert_instance_of Hash, activity.participant_hash
+  end
+
+  def test_it_has_participant
+    assert_equal 'beth', activity.participant
+  end
+
+  def test_activity_has_cost
+    assert_equal 5, activity.amount_paid
+  end
 end
